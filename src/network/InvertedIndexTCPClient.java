22a44f718a8de14a0f6f5555e798407ca6037c7b
Kosiuk, Oleksii <Oleksii.Kosiuk@McKesson.com>
2023-01-04 02:44:52 +0200

    [Feature] Implement Parallel Server and Client for using inverted index via the net in parallel mode.

---
+++ src/network/InvertedIndexTCPClient.java
@@ -0,0 +1,84 @@
+package network;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class InvertedIndexTCPClient {
+
+    private static final String NOT_ALL_PARAMS_ENTERED_MESSAGE = "Not all needed parameters(words and results file paths) " +
+            "were entered.";
+
+    private static final String SUCCESSFUL_CONNECTION_MESSAGE = "Successfully connected to the server. Starting getting " +
+            "documents from inverted index for words.\n";
+
+    private static final String DOCS_FOR_WORD_GETTING_START_MESSAGE = "Starting getting documents which contain word %s...\n";
+
+    private static final String DOCS_FOR_WORD_SUCCESSFUL_GET_MESSAGE = "Documents which contain word %s were received " +
+            "successfully.\n%s : %s.\n";
+
+    private static final String IS_PROCEEDING_NEEDED_MESSAGE = "Press N to proceed to the next word. Press E to exit.";
+
+    private static final String NOT_CORRECT_INPUT_WAS_ENTERED = "Not correct input was entered.";
+
+    private static final String SUCCESSFUL_FINISH_MESSAGE = "Documents for all words have been received successfully.\n" +
+            "Finishing work...";
+
+    private static final String EXCEPTION_MESSAGE = "Exception!!!\n";
+
+    public static void main(String[] args) {
+        final String wordsFilePath;
+        final String resultsFilePath;
+        String isProcToNextWordNeeded;
+        try {
+            wordsFilePath = args[0];
+            resultsFilePath = args[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(NOT_ALL_PARAMS_ENTERED_MESSAGE);
+            return;
+        }
+        try (Scanner consoleScanner = new Scanner(System.in);
+             FileInputStream wordsFileFIS = new FileInputStream(wordsFilePath);
+             Scanner wordsFileScanner = new Scanner(wordsFileFIS);
+             FileWriter resultsFileFW = new FileWriter(resultsFilePath);
+             Socket socket = new Socket("127.0.0.1", 4999);
+             DataOutputStream serverDOS = new DataOutputStream(socket.getOutputStream());
+             ObjectInputStream serverOIS = new ObjectInputStream(socket.getInputStream())) {
+
+            System.out.println(SUCCESSFUL_CONNECTION_MESSAGE);
+            String curWord;
+            String curDocsForWordResponse;
+            while (true) {
+                try {
+                    curWord = wordsFileScanner.nextLine();
+                } catch (NoSuchElementException e) {
+                    break;
+                }
+                System.out.println(String.format(DOCS_FOR_WORD_GETTING_START_MESSAGE, curWord));
+                serverDOS.writeUTF(curWord);
+                while ((curDocsForWordResponse = (String) serverOIS.readObject()) == null);
+                //curDocsForWordStringResult = new String(curDocsForWordByteResult);
+                resultsFileFW.write(String.format("%s : %s\n", curWord, curDocsForWordResponse));
+                System.out.println(String.format(DOCS_FOR_WORD_SUCCESSFUL_GET_MESSAGE, curWord, curWord, curDocsForWordResponse));
+                System.out.println();
+                /*System.out.println(IS_PROCEEDING_NEEDED_MESSAGE);
+                isProcToNextWordNeeded = consoleScanner.next();
+                while (!(isProcToNextWordNeeded.equals("N") || isProcToNextWordNeeded.equals("E"))) {
+                    System.out.println(NOT_CORRECT_INPUT_WAS_ENTERED);
+                    System.out.println(IS_PROCEEDING_NEEDED_MESSAGE);
+                    isProcToNextWordNeeded = consoleScanner.next();
+                }
+                if (isProcToNextWordNeeded.equals("E")) {
+                    break;
+                }*/
+            }
+            System.out.println(SUCCESSFUL_FINISH_MESSAGE);
+        } catch (Exception e) {
+            System.out.println(EXCEPTION_MESSAGE);
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/network/InvertedIndexTCPServer.java
@@ -0,0 +1,122 @@
+package network;
+
+import index.ParallelBoostedWordTextFileInvertedIndex;
+import index.building.strategy.ParallelWordTextFileInvertedIndexBuildingStrategy;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unchecked")
+public class InvertedIndexTCPServer {
+
+    private static final String NOT_ALL_PARAMS_ENTERED_MESSAGE = "Not all needed parameters(file) " +
+            "were entered.\n";
+
+    private static final String SUCCESSFUL_CONNECTION_MESSAGE = "Client %s has been successfully connected. Starting getting " +
+            "documents from inverted index for words.\n";
+
+
+
+    private static final String DOCS_FOR_WORD_SUCCESSFUL_SENT_MESSAGE = "Documents which contain word %s were got and " +
+            "sent to the client successfully.\n";
+
+    private static final String SERVER_SUCCESSFUL_FINISH_MESSAGE = "Document lists for all clients have been sent successfully.\n" +
+            "Finishing work...";
+
+    private static final String CLIENT_HANDLER_SUCCESSFUL_FINISH_MESSAGE = "Document lists for the client %s have been " +
+            "sent successfully.\nFinishing work...";
+
+    private static final String CLIENT_CONNECTION_FINISH_MESSAGE = "Client %s has been disconnected.\n";
+
+    private static final String EXCEPTION_MESSAGE = "Exception!!!\n";
+
+    private ParallelBoostedWordTextFileInvertedIndex invertedIndex;
+
+    private List<ClientHandler> clientHandlers;
+
+    public InvertedIndexTCPServer(ParallelBoostedWordTextFileInvertedIndex invertedIndex) {
+        this.invertedIndex = invertedIndex;
+        this.clientHandlers = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        final String filesToIndexDirectoryPath;
+        try {
+            filesToIndexDirectoryPath = args[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(NOT_ALL_PARAMS_ENTERED_MESSAGE);
+            return;
+        }
+        ParallelBoostedWordTextFileInvertedIndex invertedIndex = new ParallelBoostedWordTextFileInvertedIndex();
+        long startTime = System.currentTimeMillis();
+        invertedIndex.build(new ParallelWordTextFileInvertedIndexBuildingStrategy(3), filesToIndexDirectoryPath);
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - startTime));
+        System.out.println(invertedIndex.getSourcesList("all").size());
+        InvertedIndexTCPServer server = new InvertedIndexTCPServer(invertedIndex);
+        server.initAndStart();
+    }
+
+    public void initAndStart() {
+        try (ServerSocket serverSocket = new ServerSocket(4999)) {
+            int clientHandlerCurId = 0;
+            while (true) {
+                Socket clientConnectionSocket = serverSocket.accept();
+                ClientHandler curClientHandler = new ClientHandler(clientHandlerCurId, clientConnectionSocket, invertedIndex);
+                clientHandlers.add(curClientHandler);
+                curClientHandler.start();
+                System.out.println(String.format(SUCCESSFUL_CONNECTION_MESSAGE, clientConnectionSocket.getInetAddress() +
+                        ":" + clientConnectionSocket.getPort()));
+                clientHandlerCurId++;
+            }
+        } catch (IOException e) {
+            System.out.println(EXCEPTION_MESSAGE);
+            e.printStackTrace();
+        }
+        System.out.println(SERVER_SUCCESSFUL_FINISH_MESSAGE);
+    }
+
+    class ClientHandler extends Thread {
+
+        private int id;
+        private Socket clientConnectionSocket;
+        private ParallelBoostedWordTextFileInvertedIndex invertedIndex;
+
+        public ClientHandler(int id, Socket clientConnectionSocket, ParallelBoostedWordTextFileInvertedIndex invertedIndex) {
+            this.id = id;
+            this.clientConnectionSocket = clientConnectionSocket;
+            this.invertedIndex = invertedIndex;
+        }
+
+        @Override
+        public void run() {
+            try (ObjectOutputStream clientOOS = new ObjectOutputStream(clientConnectionSocket.getOutputStream());
+                 DataInputStream clientDIS = new DataInputStream(clientConnectionSocket.getInputStream())) {
+                String curWord;
+                while (true) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        break;
+                    }
+                    while ((curWord = clientDIS.readUTF()) == null);
+                    String docListStringResponse = invertedIndex.getSourcesList(curWord).toString();
+                    clientOOS.writeObject(docListStringResponse);
+                    System.out.println(String.format(DOCS_FOR_WORD_SUCCESSFUL_SENT_MESSAGE, curWord));
+                }
+            } catch (EOFException | SocketException e) { //end of data streams on client connection socket means closing of this socket from the client side
+                System.out.println(String.format(CLIENT_CONNECTION_FINISH_MESSAGE, clientConnectionSocket.getInetAddress() +
+                        ":" + clientConnectionSocket.getPort()));
+            } catch (IOException e) {
+                System.out.println("LALALALA");
+                System.out.println(EXCEPTION_MESSAGE);
+                e.printStackTrace();
+            }
+            System.out.println(String.format(CLIENT_HANDLER_SUCCESSFUL_FINISH_MESSAGE, clientConnectionSocket.getInetAddress() +
+                    ":" + clientConnectionSocket.getPort()));
+        }
+
+    }
+
+}
