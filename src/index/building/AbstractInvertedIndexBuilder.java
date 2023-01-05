c468e25f360728a01af9a862323bac60ca7146b2
Kosiuk, Oleksii <Oleksii.Kosiuk@McKesson.com>
2023-01-05 20:45:00 +0200

    [Refactoring] Use unsynchronized get instead of synchronized get when reading from already built inverted index.

--- src/index/building/AbstractInvertedIndexBuilder.java
+++ src/index/building/AbstractInvertedIndexBuilder.java
@@ -4,8 +4,6 @@ import utility.IOUtility;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public abstract class AbstractInvertedIndexBuilder implements Runnable{
     protected File[] filesToIndex;
